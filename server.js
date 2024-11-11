f1bb0ee342f74e2da2dcc75756c5f0c772325b1c
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2024-11-11 14:25:06 -0800

    setting up codebase for demo project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config({ path: './config/.env' });
+const homeRoutes = require('./routes/home');
+const connectDB = require('./config/database');
+
+connectDB()
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ bodyParser: true }));
+app.use(express.static('public'));
+
+// Routes
+app.use('/', homeRoutes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+})
